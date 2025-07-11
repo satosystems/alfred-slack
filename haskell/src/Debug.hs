f24c450c1e14bbe7a5993eee2913601ef1bf39ea
Satoshi Ogata <satosystems@gmail.com>
2025-07-11 15:23:42 +0900

    Add an debug function for chach files

--- haskell/src/Debug.hs
+++ haskell/src/Debug.hs
@@ -1,6 +1,13 @@
 module Debug
   ( debug
+  , debugWriteCacheJSON
   ) where
+
+import Data.Aeson (ToJSON, encode)
+import qualified Data.ByteString.Lazy as LB
 
 debug :: Show a => a -> IO ()
 debug = appendFile "./alfred-slack.log" . (++ "\n") . show
+
+debugWriteCacheJSON :: (ToJSON a) => a -> FilePath -> IO ()
+debugWriteCacheJSON value path = LB.writeFile path (encode value)
