89b67f20a6726ff760ca2594b878e9cdfd9f6e79
Satoshi Ogata <satosystems@gmail.com>
2025-07-14 16:57:34 +0900

    Refactor debug function

--- haskell/src/Debug.hs
+++ haskell/src/Debug.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Debug
   ( debug
   , debugWriteCacheJSON
@@ -5,10 +8,26 @@ module Debug
 
 import Data.Aeson (ToJSON, encode)
 import qualified Data.ByteString.Lazy as LB
+import qualified Data.Text as T
 import System.IO (hPutStrLn, stderr)
 
-debug :: Show a => a -> IO ()
-debug x = hPutStrLn stderr $ "\ESC[35m" ++ show x ++ "\ESC[0m"
+class DebugOutput a where
+  toString :: a -> String
+
+instance DebugOutput String where
+  toString = id
+
+instance DebugOutput Int where
+  toString = init . tail . show
+
+instance DebugOutput Bool where
+  toString = init . tail . show
+
+instance DebugOutput T.Text where
+  toString = T.unpack
+
+debug :: DebugOutput a => a -> IO ()
+debug x = hPutStrLn stderr $ "\ESC[35m" ++ toString x ++ "\ESC[0m"
 
 debugWriteCacheJSON :: (ToJSON a) => a -> FilePath -> IO ()
 debugWriteCacheJSON value path = LB.writeFile path (encode value)
