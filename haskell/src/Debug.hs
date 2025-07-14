83cfbae7bd46c8f958e436a9b17f877f1fbd7477
Satoshi Ogata <satosystems@gmail.com>
2025-07-14 13:04:06 +0900

    Change to output to standard error output instead of file output

--- haskell/src/Debug.hs
+++ haskell/src/Debug.hs
@@ -5,9 +5,10 @@ module Debug
 
 import Data.Aeson (ToJSON, encode)
 import qualified Data.ByteString.Lazy as LB
+import System.IO (hPutStrLn, stderr)
 
 debug :: Show a => a -> IO ()
-debug = appendFile "./alfred-slack.log" . (++ "\n") . show
+debug x = hPutStrLn stderr $ "\ESC[35m" ++ show x ++ "\ESC[0m"
 
 debugWriteCacheJSON :: (ToJSON a) => a -> FilePath -> IO ()
 debugWriteCacheJSON value path = LB.writeFile path (encode value)
